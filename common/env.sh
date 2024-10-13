77bbe51010186dda112929dba61a2a704d11f31e
itohdak <itohdak@gmail.com>
2024-10-14 07:24:46 +0900

    initial commit

--- common/env.sh
+++ common/env.sh
@@ -1,1 +1,6 @@
-PPROTEIN_GIT_REPOSITORY=/home/isucon/repo
+MYSQL_HOST="127.0.0.1"
+MYSQL_PORT=3306
+MYSQL_USER=isucon
+MYSQL_DBNAME=isucondition
+MYSQL_PASS=isucon
+POST_ISUCONDITION_TARGET_BASE_URL="https://isucondition-1.t.isucon.dev"
